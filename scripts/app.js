863c9ccbf5263a7a24b3c00fd31d8bd4bc69c936
sesi0001 <146714747+sesi0001@users.noreply.github.com>
2023-11-22 17:00:18 +0100

    Opdatering

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,15 @@
 let total_valid_words = [];
 
-function valid_words() {
-  let all_valid_words = document.getElementById("all_valid_words").textContent;
-
-  total_valid_words = all_valid_words.split("");
+//Fetcher alle de korrekte wordle ord fra txt-filen og kalder valid_words() funktionen med txt-filens indhold som argument
+async function load_words() {
+  const valid_words_file = await fetch("Assets/only_valid_wordle_words.txt");
+  const all_valid_words = await valid_words_file.text();
+  valid_words(all_valid_words);
 }
 
-valid_words();
+load_words();
+
+//Sætter all_valid_words som parameter i funtktionen og splitter alle dens ord i stringen indtil en array med opdelte ord
+function valid_words(all_valid_words) {
+  total_valid_words = all_valid_words.split(" ");
+}
